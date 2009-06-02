4a68fcb5046988efe8d45603c472eb59e8d443cf
shuidi008@126.com <shuidi008@126.com@fdcc8808-487c-11de-a4f5-9d9bc3506571>
2009-06-02 01:48:17 +0000

    change impl of file

--- trunk/source/OriginEngine/OEFile_Impl.cpp
+++ trunk/source/OriginEngine/OEFile_Impl.cpp
@@ -2,79 +2,74 @@
 *	created:	2009/06/01
 *	filename: 	OEFile_Impl.cpp
 *	author:		daishuidi
-	
+
 *	purpose:	
 */
 
 #include "OEFile_Impl.h"
 #include <assert.h>
 class COEFileMgr_Impl;
-COEFile_Impl::COEFile_Impl( FILE* pFile, uint nFlag )
+COEFile_Impl::COEFile_Impl(const tchar* pstrFileName, uint nFlag)
 {
-	assert(pFile);
-	m_pFile = pFile;
 	m_nFlag = nFlag;
+	m_bOK = Open(pstrFileName, nFlag);
 }
 
 COEFile_Impl::~COEFile_Impl()
 {
-	if (m_pFile)
-	{
-		fclose(m_pFile);
-		m_pFile = NULL;
-		m_nFlag = 0;
-	}
+	fclose(m_pFile);
+	m_pFile = NULL;
+	m_nFlag = 0;
 }
 
 uint COEFile_Impl::Read( void* pBuffOut, uint nSize )
 {
-	uint nResult = 0;
-	if (OFF_READ == m_nFlag && m_pFile)
-	{
-		nResult = fread(pBuffOut,sizeof(uchar),nSize,m_pFile);
-	}
-	return nResult;
+	return  fread(pBuffOut, 1, nSize, m_pFile);
 }
 
 uint COEFile_Impl::Write( const void* pBuffIn, uint nSize )
 {
-	uint nResult = 0;
-	if (OFF_WRITE == m_nFlag && m_pFile)
-	{
-		nResult = fwrite(pBuffIn,sizeof(uchar), nSize,m_pFile);
-	}
-	return nResult;
+	return fwrite(pBuffIn, 1, nSize, m_pFile);
 }
 
 uint COEFile_Impl::Seek( uint nOffset )
 {
-	uint nResult = 0;
-	if (m_pFile)
-	{
-		nResult = fseek(m_pFile, nOffset, SEEK_SET);
-	}
-	return nResult;
+	return fseek(m_pFile, nOffset, SEEK_SET);
 }
 
 uint COEFile_Impl::GetSize() const
 {
 	uint nResult = 0;
-	if (m_pFile)
-	{
-		uint nPos = ftell(m_pFile);
-		fseek(m_pFile,0,SEEK_END);
-		nResult = ftell(m_pFile);
-		fseek(m_pFile,nPos,SEEK_SET);
-	}
+	uint nPos = ftell(m_pFile);
+	fseek(m_pFile,0,SEEK_END);
+	nResult = ftell(m_pFile);
+	fseek(m_pFile,nPos,SEEK_SET);
+
 	return nResult;
 }
 
 long COEFile_Impl::Tell() const
 {
-	uint nResult = 0;
+	return ftell(m_pFile);
+}
+
+bool COEFile_Impl::Open( const tchar* pstrFileName, uint nFlag )
+{
+	assert(pstrFileName);
+	bool bResult = false;
+
+	if (OFF_READ == nFlag)
+	{
+		m_pFile = _tfopen(pstrFileName, _T("rb"));
+	}
+	else if (OFF_WRITE == nFlag)
+	{
+		m_pFile = _tfopen(pstrFileName, _T("wb"));
+	}
 	if (m_pFile)
 	{
-		nResult = ftell(m_pFile);
+		bResult = true;
 	}
-	return nResult;
+
+	return bResult;
 }
