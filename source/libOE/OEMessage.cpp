011a698a0f5fdfd72ef9b333f2eb9e48ea3c3825
zjhlogo <zjhlogo@fdcc8808-487c-11de-a4f5-9d9bc3506571>
2009-10-12 15:38:05 +0000

    + message handler

--- source/libOE/OEMessage.cpp
+++ source/libOE/OEMessage.cpp
@@ -10,11 +10,20 @@
 COEMessage::COEMessage(uint nMsgID)
 {
 	m_nMsgID = nMsgID;
+	m_pDBRead = NULL;
+	m_pDBWrite = NULL;
 }
 
 COEMessage::COEMessage(COEDataBufferRead* pDBRead)
 {
-	// TODO: 
+	m_nMsgID = OMI_UNKNOWN;
+	m_pDBRead = pDBRead;
+	m_pDBWrite = NULL;
+
+	Read(m_nMsgID);
+	UnpackData();
+
+	m_pDBRead = NULL;
 }
 
 COEMessage::~COEMessage()
@@ -25,4 +34,109 @@ COEMessage::~COEMessage()
 uint COEMessage::GetMsgID()
 {
 	return m_nMsgID;
+}
+
+bool COEMessage::ToBuffer(COEDataBufferWrite* pDBWrite)
+{
+	m_pDBWrite = pDBWrite;
+
+	if (!Write(m_nMsgID))
+	{
+		m_pDBWrite = NULL;
+		return false;
+	}
+
+	if (!PackData())
+	{
+		m_pDBWrite = NULL;
+		return false;
+	}
+
+	m_pDBWrite = NULL;
+	return true;
+}
+
+bool COEMessage::PackData()
+{
+	return true;
+}
+
+bool COEMessage::UnpackData()
+{
+	return true;
+}
+
+bool COEMessage::ReadString(tstring& strOut)
+{
+	uint nSize = 0;
+	if (!m_pDBRead->Read(&nSize, sizeof(nSize)))
+	{
+		assert(false);
+		return false;
+	}
+
+	if (nSize % sizeof(tchar) != 0)
+	{
+		assert(false);
+		return false;
+	}
+
+	uint nLength = nSize / sizeof(tchar);
+
+	if (nLength <= 0)
+	{
+		strOut.clear();
+		return true;
+	}
+
+	if (strOut.length() < nLength+1) strOut.resize(nLength+1);
+	if (!m_pDBRead->Read(&strOut[0], nSize))
+	{
+		assert(false);
+		return false;
+	}
+
+	strOut[nLength] = t('\0');
+	return true;
+}
+
+bool COEMessage::ReadBuffer(void* pBuffer, uint nSize)
+{
+	if (!m_pDBRead->Read(pBuffer, nSize))
+	{
+		assert(false);
+		return false;
+	}
+
+	return true;
+}
+
+bool COEMessage::WriteString(const tstring& strIn)
+{
+	uint nSize = strIn.length()*sizeof(tchar);
+	if (!m_pDBWrite->Write(&nSize, sizeof(nSize)))
+	{
+		assert(false);
+		return false;
+	}
+
+	if (nSize <= 0) return true;
+	if (!m_pDBWrite->Write(strIn.c_str(), nSize))
+	{
+		assert(false);
+		return false;
+	}
+
+	return true;
+}
+
+bool COEMessage::WriteBuffer(const void* pBuffer, uint nSize)
+{
+	if (!m_pDBWrite->Write(pBuffer, nSize))
+	{
+		assert(false);
+		return false;
+	}
+
+	return true;
 }
