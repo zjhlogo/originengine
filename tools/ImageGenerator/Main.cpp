4a92f302175c0c63b062963ccffef15e3d8f9360
zjhlogo <zjhlogo@fdcc8808-487c-11de-a4f5-9d9bc3506571>
2010-11-05 02:54:11 +0000

    + ImageGenerator commands (compound and extract)

--- tools/ImageGenerator/Main.cpp
+++ tools/ImageGenerator/Main.cpp
@@ -23,15 +23,17 @@ void PrintUsage()
 	static const tchar* s_szUsage = 
 		TS("Image Generator version 1.0\n")
 		TS("author: zjhlogo, zjhlogo@gmail.com\n")
-		TS("Usage: ImageGenerator.exe (output image) (RGB image) (Alpha image)\n")
-		TS("example: ImageGenerator.exe output.png diffuse.png alpha.png\n");
+		TS("Usage: ImageGenerator.exe -command parameters\n")
+		TS("    -c compound image ([in]RGB image) ([in]Alpha image) ([out]RGBA image)\n")
+		TS("    -x extract image ([in]RGBA image) ([out]Alpha image) ([out]RGB image)\n")
+		TS("example: ImageGenerator.exe -c output.png diffuse.png alpha.png\n");
 
 	std::string strUsage;
 	COEOS::tchar2char(strUsage, s_szUsage);
 	std::cout << strUsage << std::endl;
 }
 
-bool MergeImage(const tstring& strFileOut, const tstring& strFileRGB, const tstring& strFileAlpha)
+bool MergeImage(const tstring& strFileRGB, const tstring& strFileAlpha, const tstring& strFileRGBA)
 {
 	FREE_IMAGE_FORMAT fifRGB = FreeImage_GetFileType(strFileRGB.c_str());
 	FIBITMAP* dibRGB = FreeImage_Load(fifRGB, strFileRGB.c_str(), 0);
@@ -62,29 +64,62 @@ bool MergeImage(const tstring& strFileOut, const tstring& strFileRGB, const tstr
 		return false;
 	}
 
-	BOOL bOK = FreeImage_Save(FIF_PNG, dibRGB32, strFileOut.c_str());
+	BOOL bOK = FreeImage_Save(FIF_PNG, dibRGB32, strFileRGBA.c_str());
 	FreeImage_Unload(dibRGB32);
 	FreeImage_Unload(dibAlpha8);
 	return (bOK == TRUE);
 }
 
+bool ExtractImage(const tstring& strFileIn, const tstring& strFileOutRGB, const tstring& strFileOutAlpha)
+{
+	FREE_IMAGE_FORMAT fifBitmap = FreeImage_GetFileType(strFileIn.c_str());
+	FIBITMAP* dibBitmap32 = FreeImage_Load(fifBitmap, strFileIn.c_str(), 0);
+	FIBITMAP* dibBitmap24 = FreeImage_ConvertTo24Bits(dibBitmap32);
+	FIBITMAP* dibAlpha = FreeImage_GetChannel(dibBitmap32, FICC_ALPHA);
+
+	FreeImage_Save(FIF_PNG, dibBitmap24, strFileOutRGB.c_str());
+	FreeImage_Save(FIF_PNG, dibAlpha, strFileOutAlpha.c_str());
+
+	FreeImage_Unload(dibAlpha);
+	FreeImage_Unload(dibBitmap24);
+	FreeImage_Unload(dibBitmap32);
+	return true;
+}
 
 int main(int argc, char** argv)
 {
-	if (argc < 4)
+	if (argc < 5)
 	{
 		PrintUsage();
 		return 0;
 	}
 
-	tstring strOutput;
-	tstring strRGB;
-	tstring strAlpha;
+	tstring strCommand;
+	COEOS::char2tchar(strCommand, argv[1]);
+	if (strCommand == TS("-c"))
+	{
+		tstring strRGB;
+		tstring strAlpha;
+		tstring strRGBA;
 
-	COEOS::char2tchar(strOutput, argv[1]);
-	COEOS::char2tchar(strRGB, argv[2]);
-	COEOS::char2tchar(strAlpha, argv[3]);
+		COEOS::char2tchar(strRGB, argv[2]);
+		COEOS::char2tchar(strAlpha, argv[3]);
+		COEOS::char2tchar(strRGBA, argv[4]);
 
-	bool bOK = MergeImage(strOutput, strRGB, strAlpha);
+		bool bOK = MergeImage(strRGB, strAlpha, strRGBA);
+	}
+	else if (strCommand == TS("-x"))
+	{
+		tstring strRGBA;
+		tstring strRGB;
+		tstring strAlpha;
+
+		COEOS::char2tchar(strRGBA, argv[2]);
+		COEOS::char2tchar(strRGB, argv[3]);
+		COEOS::char2tchar(strAlpha, argv[4]);
+
+		bool bOK = ExtractImage(strRGBA, strRGB, strAlpha);
+	}
+
 	return 0;
 }
